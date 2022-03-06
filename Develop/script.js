dae5004fb5a3f09a8cf92129a5ff67ca53eede4a
Abeer Naeem <abeernaeem6@gmail.com>
2022-03-05 21:12:51 -0600

    Added Business Times & Color Coordination with Current Time

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,55 @@
 var timeBlockContainer = $('.timeBlock');
+var timeCont = $('#Time');
+var inputCont = $('#Input');
+var btnCont = $('#Btn');
 
 
-function TimeBlocks() {
+function setTimeBlocks() {
 
     
-    for (i=0; i < 11; i++) {
+    for (i=9; i < 21; i++) {
+        var time = moment().hour(i)
         var timeBlocks = $('<div>').addClass("row");
-        var timeBlocksTime = $('<div>').addClass("col-sm").text(i);
-        var timeBlocksInput = $('<input type="text">').addClass("col-sm").text(i);
-        var timeBlocksBtn = $('<button type="button">').addClass("col-sm");
+        var timeBlocksTime = $('<div>').addClass("col-1 text-center bg-primary word-wrap").text(time.format("hA")).attr('id', 'Time');
+        var timeBlocksInput = $('<input type="text">').addClass("col-9").text(i).attr('id', 'Input');
+        var timeBlocksBtn = $('<button type="button">').addClass("col-2").attr('id', 'Btn');
 
          timeBlockContainer.append(timeBlocks);
          timeBlocks.append(timeBlocksTime);
          timeBlocks.append(timeBlocksInput);
          timeBlocks.append(timeBlocksBtn);
 
-                                    
+        var currentTime = moment().hour();
+
         
+         if (time.format("H") < currentTime) {
+             timeBlocksInput.css('background-color', 'red');
+         } else if (time.format("H") == currentTime) {
+            timeBlocksInput.css('background-color', 'green');
+         } else {
+            timeBlocksInput.css('background-color', 'blue');
+         }
+
+         
     }
     
-
-
+  
 }
 
-TimeBlocks()
+
+function setTime() {
+    currentTime = moment().hour()
+    if (time < currentTime) {
+        inputCont.css('background-color', 'red');
+    } else if (time === currentTime) {
+        inputCont.css('background-color', 'green');
+    } else {
+        inputCont.css('background-color', 'blue');
+    }
+}
+
+
+
+
+
+setTimeBlocks()
