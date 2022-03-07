36b78299ab3f30be4d8b3d828aa95a1d0d1a9a96
Abeer Naeem <abeernaeem6@gmail.com>
2022-03-06 22:38:01 -0600

    Added Local Storage Feature

--- Develop/script.js
+++ Develop/script.js
@@ -1,55 +1,55 @@
-var timeBlockContainer = $('.timeBlock');
-var timeCont = $('#Time');
-var inputCont = $('#Input');
-var btnCont = $('#Btn');
+var timeBlockContainer = $(".timeBlock");
 
+
+setTimeBlocks();
 
 function setTimeBlocks() {
+  for (i = 9; i < 21; i++) {
+    var time = moment().hour(i);
+    var timeBlocks = $("<div>").addClass("row");
+    var timeBlocksTime = $("<div>")
+      .addClass("col-1 text-center bg-primary word-wrap")
+      .text(time.format("hA"))
+      .attr("id", "Time");
+    var timeBlocksInput = $('<input type="text">')
+      .addClass(`col-9 ${i}`)
+      .text(i)
+      .attr("id", `${i}-input`);
+    var timeBlocksBtn = $('<button>').addClass(`col-2 Btn`).attr("id", `${i}`);
 
-    
-    for (i=9; i < 21; i++) {
-        var time = moment().hour(i)
-        var timeBlocks = $('<div>').addClass("row");
-        var timeBlocksTime = $('<div>').addClass("col-1 text-center bg-primary word-wrap").text(time.format("hA")).attr('id', 'Time');
-        var timeBlocksInput = $('<input type="text">').addClass("col-9").text(i).attr('id', 'Input');
-        var timeBlocksBtn = $('<button type="button">').addClass("col-2").attr('id', 'Btn');
+    timeBlockContainer.append(timeBlocks);
+    timeBlocks.append(timeBlocksTime);
+    timeBlocks.append(timeBlocksInput);
+    timeBlocks.append(timeBlocksBtn);
 
-         timeBlockContainer.append(timeBlocks);
-         timeBlocks.append(timeBlocksTime);
-         timeBlocks.append(timeBlocksInput);
-         timeBlocks.append(timeBlocksBtn);
+    var currentTime = moment().hour();
 
-        var currentTime = moment().hour();
-
-        
-         if (time.format("H") < currentTime) {
-             timeBlocksInput.css('background-color', 'red');
-         } else if (time.format("H") == currentTime) {
-            timeBlocksInput.css('background-color', 'green');
-         } else {
-            timeBlocksInput.css('background-color', 'blue');
-         }
-
-         
-    }
-    
-  
-}
-
-
-function setTime() {
-    currentTime = moment().hour()
-    if (time < currentTime) {
-        inputCont.css('background-color', 'red');
-    } else if (time === currentTime) {
-        inputCont.css('background-color', 'green');
+    if (time.format("H") < currentTime) {
+      timeBlocksInput.css("background-color", "red");
+    } else if (time.format("H") == currentTime) {
+      timeBlocksInput.css("background-color", "green");
     } else {
-        inputCont.css('background-color', 'blue');
+      timeBlocksInput.css("background-color", "blue");
+    }
+  }
+
+  storage()
+}
+
+function storage() {
+    for (i=9;i<21;i++) {
+        $(`#${i}-input`).val(localStorage.getItem(`${i}`))
     }
 }
 
 
 
+$(".Btn").on("click", function (event) {
+    var id = event.target.id;
+    for (i=9;i<21;i++) {
+        if (event.target.id == i)
+        localStorage.setItem(`${i}`, $(`#${i}-input`).val())
+    }
 
+  });
 
-setTimeBlocks()
